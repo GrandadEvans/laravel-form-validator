d88d4e36067df9215b7734155eab40c3eec6f9c1
John Evans <john@grandadevans.com>
2014-10-07 02:20:52 +0100

    Update to the base path rather than the workbench path

--- tests/codeception/acceptance/formGeneratorCept.php
+++ tests/codeception/acceptance/formGeneratorCept.php
@@ -17,20 +17,18 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('create a form taking into account all valid parameters');
 
 $base_dir = '../../../';
-$package_dir = $base_dir . 'workbench/grandadevans/generate-form';
-
 
 
 /*
  * Test all options
  */
-$command = 'php ' . $base_dir . 'artisan generate:form Foo --dir="' . $package_dir . '/tests/codeception/actualTestResults" --namespace="Bar" --rules="baz|required|email & qux|between:3,6"';
+$command = 'php ' . $base_dir . 'artisan generate:form Foo --dir="' . $base_dir . '/tests/codeception/actualTestResults" --namespace="Bar" --rules="baz|required|email & qux|between:3,6"';
 $I->runShellCommand($command);
 
 $I->seeInShellOutput('Form has been saved to');
 
-$I->openFile($package_dir . '/tests/codeception/actualTestResults/FooForm.php');
-$I->canSeeFileContentsEqual(file_get_contents($package_dir .
+$I->openFile($base_dir . '/tests/codeception/actualTestResults/FooForm.php');
+$I->canSeeFileContentsEqual(file_get_contents($base_dir .
     '/tests/codeception/expectedTestResults/FooForm-with-all-options.php'));
 
 unlink('tests/codeception/actualTestResults/FooForm.php');
@@ -40,13 +38,13 @@ unlink('tests/codeception/actualTestResults/FooForm.php');
 /*
  * Test no namespace and no rules
  */
-$command = 'php ' . $base_dir . 'artisan generate:form Foo --dir="' . $package_dir . '/tests/codeception/actualTestResults"';
+$command = 'php ' . $base_dir . 'artisan generate:form Foo --dir="' . $base_dir . '/tests/codeception/actualTestResults"';
 $I->runShellCommand($command);
 
 $I->seeInShellOutput('Form has been saved to');
 
-$I->openFile($package_dir . '/tests/codeception/actualTestResults/FooForm.php');
-$I->canSeeFileContentsEqual(file_get_contents($package_dir .
+$I->openFile($base_dir . '/tests/codeception/actualTestResults/FooForm.php');
+$I->canSeeFileContentsEqual(file_get_contents($base_dir .
       '/tests/codeception/expectedTestResults/FooForm-with-no-namespace-and-no-rules.php'));
 
 unlink('tests/codeception/actualTestResults/FooForm.php');
@@ -61,8 +59,8 @@ $I->runShellCommand($command);
 
 $I->seeInShellOutput('Form has been saved to');
 
-$I->openFile($package_dir . '/app/Forms/FooForm.php');
-$I->canSeeFileContentsEqual(file_get_contents($package_dir .
+$I->openFile($base_dir . '/app/Forms/FooForm.php');
+$I->canSeeFileContentsEqual(file_get_contents($base_dir .
       '/tests/codeception/expectedTestResults/FooForm-with-no-namespace-and-no-rules-or-dir.php'));
 
 unlink('app/Forms/FooForm.php');
